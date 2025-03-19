ceea3c002e52eba178b1b6dab01544c2433a7137
jagadeesh-431 <jagadeeshtmr43@gmail.com>
2025-03-19 19:32:15 +0530

    Add files via upload
    dark and light mode for web page

---
+++ app.js
@@ -0,0 +1,10 @@
+var body =document.querySelector("body");
+function dark_mode(){
+body.className="";
+body.classList.add("dark");
+}
+function light_mode(){
+body.className="";
+body.classList.add(light);
+}
+    
